dfd7185eee1bb1a5d23277d7c5a9327fb405b075
Shunxian Cai <shucai@microsoft.com>
2016-12-14 19:14:22 -0800

    update to new nuspec, save them in the uploads folder

--- script.py
+++ script.py
@@ -9,43 +9,53 @@ feedUrl = 'http://www.siteextensions.net/api/v2'
 siteExtensionTag = 'siteextension'
 
 def editNupkg(listTuples):
-    if not os.path.exists('nuspecs'):
-        os.makedirs('nuspecs')
+    if not os.path.exists('uploads'):
+        os.makedirs('uploads')
         for packageId,version in listTuples:
             downloadName = packageId+'.'+version+'.nupkg' # sometimes
-            print('try to extracts '+downloadName)
-            with zipfile.ZipFile('packages/'+downloadName) as zip_ref:
-                nuspecName = packageId+'.nuspec'
-                print('extracts ' + nuspecName)
-                zip_ref.extract(nuspecName,'nuspecs')
-    #FIXME do not seperate these steps, pipeline them all together
-    for packageId,version in listTuples:
-        nuspecName = packageId+'.nuspec'
-        relativeFileName = 'nuspecs/'+nuspecName
-        with open(relativeFileName,'r+b') as f: #encoding utf-8, not ASCII
-            tree = etree.parse(f)
-            f.seek(0,0) # reset the file position
-            root = tree.getroot()
-            etree.register_namespace('',root.tag[1:root.tag.index('}')])
-            xmlns = {'d':root.tag[1:root.tag.index('}')]}#{http://schemas.microsoft.com/packaging/2010/07/nuspec.xsd}package
-            metaData = root.find('d:metadata',xmlns)
-            # add packageType
-            packageTypes = etree.SubElement(metaData,'packageTypes')
-            packageType = etree.SubElement(packageTypes,'packageType',attrib={'name':'AzureSiteExtension'})
-            tags = metaData.find('d:tags',xmlns)
-            if tags is None: # xml.Element len() shows how many children
-                tags = etree.SubElement(metaData,'tags')
-                tags.text = ''
-                tags.tail = os.linesep
-            if not siteExtensionTag in tags.text:
-                tagsList = tags.text.split()
-                tagsList.append(siteExtensionTag)
-                tags.text = ' '.join(tagsList)
-            tree.write(f,xml_declaration=True)
+            nuspecName = packageId+'.nuspec'
+            with zipfile.ZipFile('packages/'+downloadName) as download:
+                with zipfile.ZipFile('uploads/'+downloadName,'w') as upload:
+                    for zipI in download.infolist(): # zipinfo has compression type
+                        if zipI.filename != nuspecName:
+                            upload.writestr(zipI,download.read(zipI.filename))#not zipped
+                    with download.open(nuspecName) as f:
+                        # edit nuspec
+                        tree = etree.parse(f)
+                        root = tree.getroot()
+                        etree.register_namespace('',root.tag[1:root.tag.index('}')])
+                        xmlns = {'d':root.tag[1:root.tag.index('}')]}#{http://schemas.microsoft.com/packaging/2010/07/nuspec.xsd}package
+                        metaData = root.find('d:metadata',xmlns)
+                        siblingTail = metaData[0].tail
+                        # insert at position 1
+                        # add packageType
+                        packageTypes = etree.Element('packageTypes')
+                        packageTypes.text = siblingTail+'  '
+                        packageType = etree.SubElement(packageTypes,'packageType',attrib={'name':'AzureSiteExtension'})
+                        packageType.tail = siblingTail
+                        packageTypes.tail = siblingTail
+                        metaData.insert(0,packageTypes)
+                        tags = metaData.find('d:tags',xmlns)
+                        if tags is None: # xml.Element len() shows how many children
+                            tags = etree.Element('tags')
+                            tags.text = ''
+                            tags.tail = siblingTail
+                            metaData.insert(0,tags)
+                        if not siteExtensionTag in tags.text:
+                            #TODO nodediag.nuspec has tag SiteExtension
+                            tagsList = tags.text.split()
+                            tagsList.append(siteExtensionTag)
+                            tags.text = ' '.join(tagsList)
+
+                        upload.writestr(nuspecName,etree.tostring(root))
 
 def downLoadPackages():
     output = subprocess.check_output(['nuget.exe','list','-source',feedUrl],universal_newlines=True) # does not always return the same thing
     listTuples = [lines.split() for lines in output.splitlines()]
+    while listTuples[0][1] != '2.2.0':
+        # stupid solution
+        output = subprocess.check_output(['nuget.exe','list','-source',feedUrl],universal_newlines=True)
+        listTuples = [lines.split() for lines in output.splitlines()]
     if not os.path.exists('packages'):
         os.makedirs('packages')
         for packageId,version in listTuples:
